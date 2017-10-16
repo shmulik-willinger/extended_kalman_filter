b36edd9ecca25539200503110f176c7222b16027
ShmuliShmulik <shmulik83@gmail.com>
2017-10-16 09:27:23 +0300

    initializing the project

---
+++ src/tools.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  /**
+  TODO:
+    * Calculate the RMSE here.
+  */
+	VectorXd temp(4);
+	temp << 1, 2, 3, 4;
+	return temp;
+}
+
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+  /**
+  TODO:
+    * Calculate a Jacobian here.
+  */
+	MatrixXd temp(3, 4);
+	return temp;
+}
